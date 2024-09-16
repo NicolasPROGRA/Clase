788b1bef53c5660bd7a057fda190c28451a32ec2
Nicolas C <nicolasc101004@gmail.com>
2024-09-16 11:30:02 -0300

    cambio en prestamo f_devolucion

--- biblioteca-clases/pestramo.py
+++ biblioteca-clases/pestramo.py
@@ -1,8 +1,9 @@
 class prestamo :
-    def __init__(selft,n_identificador,isbn,f_entrega,entregado):
+    def __init__(selft,n_identificador,isbn,f_entrega,f_devolucion):
         selft.__n_identificador= n_identificador,
         selft.__isbn=isbn,
-        selft._f_entrega=f_entrega,
-        selft.__entregado= entregado
+        selft.__f_entrega=f_entrega,
+        selft.__f_devolucion= f_devolucion
+        
         
         
