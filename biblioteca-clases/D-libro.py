e178e67f0b90038330cac513f8360ed130044a74
Nicolas C <nicolasc101004@gmail.com>
2024-09-23 11:31:20 -0300

    a

--- biblioteca-clases/D-libro.py
+++ biblioteca-clases/D-libro.py
@@ -2,7 +2,9 @@ import libro
 import editorial
 
 class D_libro:
-    def __init__(self,categoria,n_paginas):
+    def __init__(self,categoria,n_paginas,isbn,id_editorial):
+        libro.__init__(isbn)
+        editorial.__init__(id_editorial)
         self.categoria = categoria,
         self.n_paginas = n_paginas
          
--- biblioteca-clases/editorial.py
+++ biblioteca-clases/editorial.py
@@ -1,4 +1,5 @@
 class editorial:
-    def _init_(self,indice,nombre):
+    def __init__(self,indice,nombre,id_editorial):
         self.nombre=nombre,
-        self.indice= indice
+        self.indice= indice,
+        self.id_editorial= id_editorial
