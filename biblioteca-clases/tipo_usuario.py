a743a3592c4940029ec77c99f939b2bf04e8fb23
NicolasCB <167951159+NicolasPROGRA@users.noreply.github.com>
2024-10-07 11:34:58 -0300

    clase nueva

---
+++ biblioteca-clases/tipo_usuario.py
@@ -0,0 +1,6 @@
+class tipo_de_usuario:
+    def __init__ (self,id_tipo_usuario,tipo_usuario,descripcion_usuario):
+        self.id_tipo_usuario=id_tipo_usuario
+        self.tipo_usuario=tipo_usuario
+        self.descripcion_usuario=descripcion_usuario
+        
